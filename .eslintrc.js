5b304c1c456ac30fa22319f1441e10796958ed11
AdamFielding <adamajfielding@gmail.com>
2019-07-21 18:46:50 +0100

    Specify react version for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,10 @@ module.exports = {
     "react/prop-types": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 };
