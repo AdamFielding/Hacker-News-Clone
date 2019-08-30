3ea549aef6e780e94e9f9ad91c8b682662c62b61
Adam Fielding <adamajfielding@gmail.com>
2019-08-30 16:17:51 +0100

    Add redux boiler plate and wrap app in provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,9 @@ import { Container } from "semantic-ui-react";
 import { Stories } from "./Stories";
 import { Title } from "./Title";
 import ViewSwitchButton from "./ViewSwitchButton";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { appReducer } from "../store/configureStore";
 
 export const enum View {
   list,
@@ -20,14 +23,16 @@ export class App extends React.PureComponent {
 
   public render(): JSX.Element {
     return (
-      <Container style={{ marginTop: "3em", marginBottom: "3em" }}>
-        <Title title="Hacker News Clone" />
-        <ViewSwitchButton
-          view={this.state.view}
-          onClick={this.toggleStoryView}
-        />
-        <Stories view={this.state.view} />
-      </Container>
+      <Provider store={createStore(appReducer)}>
+        <Container style={{ marginTop: "3em", marginBottom: "3em" }}>
+          <Title title="Hacker News Clone" />
+          <ViewSwitchButton
+            view={this.state.view}
+            onClick={this.toggleStoryView}
+          />
+          <Stories view={this.state.view} />
+        </Container>
+      </Provider>
     );
   }
 
